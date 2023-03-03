7cde3d9359edd6af37277285479961d76ecafe14
marcuszz <marcuzcalaguas@gmail.com>
2023-03-03 10:24:47 +0800

    commit

---
+++ pdc03_mod2/pdc03_mod2/activity5.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Collections.ObjectModel;
+
+
+namespace pdc03_mod2
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class activity5 : ContentPage
+	{
+		ObservableCollection<employee> employees = new ObservableCollection<employee>();
+		public ObservableCollection<employee> Employees { get { return employees; } }
+
+		public activity5 ()
+		{
+			InitializeComponent ();
+
+			{
+				lst.ItemsSource = employees;
+				employees.Add(new employee { DisplayName = "Juan Dela Cruz", Position = "President", profileImage = "image1.jpg" });
+                employees.Add(new employee { DisplayName = "John Dela Cruz", Position = "Vice President", profileImage = "image2.jpg" });
+                employees.Add(new employee { DisplayName = "Janno Dela Cruz", Position = "Secretary", profileImage = "image03.jpg" });
+            }
+		}
+	}
+}
---
+++ pdc03_mod2/pdc03_mod2/employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pdc03_mod2
+{
+    public class employee
+    {
+        public string DisplayName { get; set; }
+        
+        public string Position { get; set; }
+
+        public string profileImage { get; set; }
+    }
+}
